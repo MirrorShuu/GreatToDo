122fd30741f285797e4e52f30c75886f4d06c57b
MirrorSku <steparik049@gmail.com>
2022-11-21 21:49:40 +0300

    add layout

--- app/src/main/java/com/example/greattodo/ListFragment.kt
+++ app/src/main/java/com/example/greattodo/ListFragment.kt
@@ -6,17 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.greattodo.databinding.FragmentFirstBinding
+import com.example.greattodo.databinding.ListFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+class ListFragment : Fragment() {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: ListFirstBinding? = null
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +20,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = ListFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +28,12 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.btmCreate.setOnClickListener {
+            findNavController().navigate(R.id.action_ListFragment_to_SecondFragment)
         }
+//        binding.btmSetting.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_Setting_Fragment)
+//        }
     }
 
     override fun onDestroyView() {
